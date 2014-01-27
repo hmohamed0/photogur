4e13b1cff3c2b05bead85692ad7088ad2f92b6f6
Hodo Mohamed <hmohamed0@gmail.com>
2014-01-27 18:00:28 -0500

    Configuring root as pictures#index

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,14 @@
 class PicturesController < ApplicationController
 
+  
+def index
+    @pictures = Picture.all
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
   def new
     @picture = Picture.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Photogur::Application.routes.draw do
- get 'pictures' => 'pictures#index'
-
-  post 'pictures' => 'pictures#create'
-  get 'pictures/new' => 'pictures#new'
-  get 'pictures/:id' => 'pictures#show'
-
+ Photogur::Application.routes.draw do
+  root :to => "pictures#index"
+  get 'pictures' => 'pictures#index'
+  
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
